01cfbef48d16eb5001f745ad2b268b1a1ea6192d
venom <abdo30201080101694@gmail.com>
2023-10-03 23:49:17 +0300

    v 2.0.0

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 // let dataArea = document.querySelector('.data');
 
-
 // searchButton.onclick = () => {
 //     getRepos();
 // }
@@ -30,9 +29,9 @@
 
 // import axios from 'axios';
 
-const searchArea = document.querySelector('.search-area input');
-const searchButton = document.querySelector('.search-area .search-button');
-const dataArea = document.querySelector('.data');
+const searchArea = document.querySelector(".search-area input");
+const searchButton = document.querySelector(".search-area .search-button");
+const dataArea = document.querySelector(".data");
 
 searchButton.onclick = () => {
   getRepos();
@@ -40,22 +39,49 @@ searchButton.onclick = () => {
 
 const getRepos = () => {
   const username = searchArea.value.trim(); // Trim to remove leading/trailing whitespace
-  if (username === '') {
-    dataArea.innerHTML = '<span>Please Enter a GitHub Username</span>';
-    dataArea.style.textAlign = 'center';
-    dataArea.style.fontWeight = 'bold';
+  if (username === "") {
+    dataArea.innerHTML = "<span>Please Enter a GitHub Username</span>";
+    dataArea.style.textAlign = "center";
+    dataArea.style.fontWeight = "bold";
   } else {
-    fetch(`https://api.github.com/users/abdo100300500/repos`)
-    .then((res) => {
-                    return res.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                })
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        dataArea.innerHTML = "";
+        data.forEach((repo) => {
+          console.log(repo.name);
+          let mainDiv = document.createElement("div");
+          mainDiv.className = "repo-box";
+          mainDiv.textContent = repo.name;
+
+          let url = document.createElement("a");
+          url.className = "button";
+          url.href = `https://github.com/${username}/${repo.name}`;
+          url.setAttribute("target", "_blank");
+          url.textContent = "visit";
+
+          let startSpan = document.createElement("span");
+          startSpan.className = "button";
+          startSpan.textContent = `Stars ${repo.stargazers_count}`;
+
+          let buttons = document.createElement("div");
+          buttons.className = "buttons";
+          buttons.appendChild(url);
+          buttons.appendChild(startSpan);
+
+          mainDiv.appendChild(buttons);
+
+          dataArea.appendChild(mainDiv);
+        });
+      })
       .catch((error) => {
+        console.error(error);
         dataArea.innerHTML = `<span>Error: ${error.message}</span>`;
-        dataArea.style.textAlign = 'center';
-        dataArea.style.fontWeight = 'bold';
+        dataArea.style.textAlign = "center";
+        dataArea.style.fontWeight = "bold";
       });
   }
 };
