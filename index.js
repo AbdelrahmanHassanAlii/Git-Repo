f16c528b4042f85f058717be7dbcb77fb6709779
venom <abdo30201080101694@gmail.com>
2024-02-15 00:39:17 +0200

    finish above 20% of card content

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const searchButton = document.querySelector(".search-area .search-button");
 const dataArea = document.querySelector(".data");
 const card = document.querySelector(".card");
 
+window.onload = () => {
+  if (card.innerHTML.trim() === "") {
+    card.style.display = "none";
+  }
+};
+
+//function to change format of given date
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-US", options);
+  return `Joined   ${formattedDate}`;
+}
+
 searchButton.onclick = () => {
   getUserData();
   getRepos();
@@ -20,7 +34,50 @@ const getUserData = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data.avatar_url);
+        //display the card
+        card.style.display = "block";
+        //create main-info component
+        let mainInfo = document.createElement("div");
+        mainInfo.className = "main-info";
+
+        //create the image
+        let image = document.createElement("img");
+        image.className = `user-avatar`;
+        image.src = data.avatar_url;
+
+        //create the text container next to image
+        let text = document.createElement("div");
+        text.className = "text";
+
+        //get the name of the user
+        let name = document.createElement("p");
+        name.className = "name";
+        name.innerHTML = data.name;
+
+        //get the user handle
+        let handle = document.createElement("a");
+        handle.href = data.html_url;
+        handle.target = "_blank";
+        handle.className = "handle";
+        handle.innerHTML = "@" + data.login;
+
+        //get the user Joined date
+        let date = document.createElement("p");
+        date.className = "date";
+        date.innerHTML = formatDate(data.created_at);
+
+        //append the name to taxt area
+        text.appendChild(name);
+        text.appendChild(handle);
+        text.appendChild(date);
+        //append image to main-info
+        mainInfo.appendChild(image);
+        mainInfo.appendChild(text);
+
+        //append main-info to card
+        card.appendChild(mainInfo);
+
+        searchArea.innerHTML = ``;
       });
   }
 };
